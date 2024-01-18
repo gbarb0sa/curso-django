25c38576a106e85386553950b43ae6547b9a7b4d
Gustavo Barbosa de Souza <gbs.gust@gmail.com>
2024-01-17 22:30:21 -0300

    Update styles.css and recipe.html, and filter recipes by is_published in views.py

--- recipes/views.py
+++ recipes/views.py
@@ -31,7 +31,11 @@ def category(request, category_id):
 
 
 def recipe(request, id):
-    recipe = Recipe.objects.get(id=id)
+    recipe = Recipe.objects.filter(
+        pk=id,
+        is_published=True
+    ).order_by('-id').first()
+
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
         'is_detail_page': True,
