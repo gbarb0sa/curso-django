96eb72cfb2e5cdaebd32adeaea33632143b5bfbf
Gustavo Barbosa de Souza <gbs.gust@gmail.com>
2024-01-18 16:08:28 -0300

    rename project

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_list_or_404, render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from recipes.models import Recipe
 
@@ -6,7 +6,8 @@ from recipes.models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.filter(is_published=True).order_by('-id')[:10]
+    recipes = Recipe.objects.filter(
+        is_published=True).order_by('-id')
 
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
@@ -15,7 +16,6 @@ def home(request):
 
 
 def category(request, category_id):
-
     recipes = get_list_or_404(
         Recipe.objects.filter(
             category__id=category_id,
@@ -31,10 +31,7 @@ def category(request, category_id):
 
 
 def recipe(request, id):
-    recipe = Recipe.objects.filter(
-        pk=id,
-        is_published=True
-    ).order_by('-id').first()
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True)
 
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
